42da62cca6f17d1499b701aff7ba501155061335
Sri809 <dharshinisri443@gmail.com>
2024-09-20 12:54:08 +0530

    Update main.c

--- HelloWorld/main.c
+++ HelloWorld/main.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
-
-/*
- * @author jelathro
- * @date 2/18/13
- * 
- * Print "Hello, World!" to the console
- */
-int main(void){
-    printf("Hello, World!\n");
+int main()
+{
+  float x,y,z,c;
+printf("enter the x,y,z");
+scanf("%f %f %f",&x,&y,&z);
+c=x+y+z;
+printf("%f",c)
     return 0;
 }
